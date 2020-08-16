5ae99b40df7b8a759b50422b77367612d93df5c2
Chethan V <chethan.v@PFIN.COM>
2020-08-16 20:37:49 +0530

    update

---
+++ src/main/java/com/qa/mypages/Page.java
@@ -0,0 +1,32 @@
+package com.qa.mypages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class Page {
+	WebDriver driver;
+	WebDriverWait wait;
+	
+	public Page(WebDriver driver){
+		this.driver = driver;
+		this.wait = new WebDriverWait(this.driver, 15);
+	}
+	
+	//abstract methods
+	public abstract String getPageTitle();
+	public abstract String getPageHeader();
+	public abstract WebElement getElement(By locator);
+	public abstract void waitForElementPresent(By locator);
+	public abstract void waitForPageTitle(String Title);
+	
+	//using java generic concept
+	public<TPage extends BasePage> TPage getInstance(Class<TPage>pageClass){
+		try{
+			return pageClass.getDeclaredConstructor(WebDriver.class).newInstance(this.driver);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
